0a1d9cb3e43ee30af496c24ee728122a69713582
Mikael Elias <129218135+mikaelelias@users.noreply.github.com>
2023-06-03 15:20:04 -0300

    Init Javascript

---
+++ main.js
@@ -0,0 +1,10 @@
+function btn_modal_validation() {
+    let modal_open = document.querySelector(".modal");
+    modal_open.style.display = "block"
+}
+
+function close_btn() {
+    let close_modal = document.querySelector(".modal");
+    close_modal.style.display = "none"
+}
+
