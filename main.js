5ee678aa8ac6d230c07f31666c4cd482610f3a0f
Mikael Elias <129218135+mikaelelias@users.noreply.github.com>
2023-06-04 15:34:41 -0300

    js

--- main.js
+++ main.js
@@ -3,10 +3,6 @@ function btn_modal_validation_open() {
     modal_open.style.display = "block"
 }
 
-function close_html() {
-    let close_email_field = document.querySelector(".email_field");
-    close_email_field.style.display ="none";
-  }
 
 function btn_modal_validation() {
     const emailInput = document.querySelector(".input-email");
@@ -24,23 +20,25 @@ function btn_modal_validation() {
       emailInput.classList.add("error");
       // Exemplo de exibição de uma mensagem de erro
       alert("Por favor, insira um endereço de email válido.");
-    }
+    } 
 }
-
-  var capturing = "";
-
-  function capture(){
-      capturing = document.querySelector(".input-email").value;
-      document.getElementById('id_email').innerHTML = capturing;
-  }
-  
-  function close_btn() {
-    let modal = document.querySelector(".modal");
-    modal.style.display = "none";
-  }
 
 function close_btn() {
-    let close_modal = document.querySelector(".modal");
-    close_modal.style.display = "none"
+  let close_modal = document.querySelector("btn_dismiss");
+  close_modal.style.display = "none";
 }
+
+
+/*
+var capturing = "";
+  function capture(){
+      capturing = document.querySelector(".input-email").value;
+      document.getElementById('id_email').innerHTML = capturing = capture
+  }
+  
+  function close_html() {
+    let close_email_field = document.querySelector(".email_field");
+    close_email_field.style.display ="none";
+  }
+  */
 
